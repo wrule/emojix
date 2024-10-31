b52d0e7ae0478d7750fd497f39526464f18fc8f0
jimao <shihao.gu@perfma.com>
2024-10-31 16:38:29 +0800

    prompt

--- app/prompt.tsx
+++ app/prompt.tsx
@@ -1,5 +1,11 @@
 // 我们正在开发一个nextjs react的ssr应用
+// react相关依赖的版本是
+// "react": "^18",
+// "react-dom": "^18",
 // 我们使用typescript（也就是.tsx）来编写页面和组件
+// typescript相关依赖的版本是
+// "tsx": "^4.19.0",
+// "typescript": "^5.6.2"
 // 基本的样式我们只使用tailwindcss来做，tailwindcss版本是
 // "tailwindcss": "^3.4.14",
 // 对于动画相关的设计，你可以使用以下依赖
@@ -10,7 +16,8 @@
 // "gsap": "^3.12.5",
 // "three": "^0.137.0",
 // 其他相关的三方依赖。你可以使用
-
+// "dayjs": "^1.11.13",
+// "nanoid": "^5.0.8",
 
 export default
 function AI() {
