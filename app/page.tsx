3ceba6ff2316cd77ab1d98210d7c2af4fa1beabe
jimao <shihao.gu@perfma.com>
2024-10-31 16:16:24 +0800

    tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,9 @@ export default function App() {
   return (
     <main>
       <h1>My todos</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
