7cd70636f3645400d6b220f85cd06e9aa1cea456
jimao <shihao.gu@perfma.com>
2024-10-31 16:34:01 +0800

    prompt

---
+++ app/prompt.tsx
@@ -0,0 +1,15 @@
+// 我们正在开发一个nextjs的ssr应用
+// 基本的样式我们只使用tailwindcss来做，tailwindcss版本是
+// "tailwindcss": "^3.4.14",
+// 对于动画相关的设计，你可以使用以下依赖
+// "@gsap/react": "^2.1.1",
+// "@react-three/fiber": "^8.2.2",
+// "framer-motion": "^11.11.10",
+// "framer-motion-3d": "^11.2.0",
+// "gsap": "^3.12.5",
+// "three": "^0.137.0",
+
+export default
+function AI() {
+  return <div></div>;
+}
